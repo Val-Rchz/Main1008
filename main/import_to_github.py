7b9d79d697aaaf40ec2345eb9021931f6d76f4dd
PaulBODINVARENNE <paulbodin@free.fr>
2024-12-03 16:09:22 +0100

    Vtemp

--- main/import_to_github.py
+++ main/import_to_github.py
@@ -1,19 +1,16 @@
 import os
 import subprocess
 
-# Configuration
-repo_url = "https://github.com/TON-UTILISATEUR/TON-DÉPÔT.git"  # Remplace par ton URL GitHub
-file_to_add = "Test.json" 
-commit_message = "Add example.json file"  # Message du commit
 
-# Clone le dépôt si nécessaire
-if not os.path.exists("TON-DÉPÔT"):
+repo_url = "https://github.com/Val-Rchz/Main1008/tree/main/main.git"  
+file_to_add = "Test.json" 
+commit_message = "Add example.json file" 
+
+if not os.path.exists("Test"):
     subprocess.run(["git", "clone", repo_url])
 
-# Se déplacer dans le dossier du dépôt
-os.chdir("TON-DÉPÔT")
+os.chdir("Test")
 
-# Ajouter le fichier au dépôt
 subprocess.run(["git", "add", file_to_add])
 subprocess.run(["git", "commit", "-m", commit_message])
 subprocess.run(["git", "push", "origin", "main"])
